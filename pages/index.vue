1f534c906d9386542c2df5820a778b6b8882400e
KevinHoi128 <krose.kachon@gmail.com>
2022-01-08 22:18:56 +0800

    tbc

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-container fluid>
       <v-col cols="12">
         <v-row justify="center">
-          <!-- <h2>網站仍在建設中，或者先看看簡歷?</h2> -->
+          <!-- <h2>網站仍在建設中，或者先看看簡歷?1</h2> -->
         </v-row>
         <!-- <v-row justify="center">
           <v-btn link to="/Resume">Resume</v-btn>
